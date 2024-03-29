b251a2aec0bd01b58ab5c85710a46159561b9a2b
iamwinner422 <iamwinner422@gmail.com>
2024-03-29 22:48:27 +0000

    finishing the register user controller

--- app/controllers/users_controller.ts
+++ app/controllers/users_controller.ts
@@ -1,7 +1,17 @@
 import type { HttpContext } from '@adonisjs/core/http'
+import {RegisterValidator} from "#validators/user";
+import User from "#models/user";
 
 export default class UsersController {
   async login({ request, response}: HttpContext){
   }
-
+  async register({ request, response}: HttpContext){
+    const payload = await request.validateUsing(RegisterValidator)
+    const userRaw = await User.create(payload)
+    const user = userRaw.serialize()
+    delete user.id
+    delete user.password
+    delete user.role
+    return response.status(201).send({success: true, data: user})
+  }
 }
