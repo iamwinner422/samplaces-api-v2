b1e355e5968ba94b52aa62bbb3a3eb75b7aaaeb7
iamwinner422 <iamwinner422@gmail.com>
2024-03-29 22:39:39 +0000

    finition de la création de la  validation de register

--- app/validators/user.ts
+++ app/validators/user.ts
@@ -13,9 +13,18 @@ const identifier = vine.union([
   vine.union.if((value) => hasType(value, 'email'), emailIdentifier),
   vine.union.if((value) => hasType(value, 'username'), usernameIdentifier)
 ])
+
 export const LoginValidator = vine.compile(
   vine.object({
     identifier: vine.array(identifier),
     password: vine.string().trim()
+  })
+)
+
+export const RegisterValidator = vine.compile(
+  vine.object({
+    username: vine.string().trim().toLowerCase(),
+    email: vine.string().trim().email().toLowerCase(),
+    password: vine.string().trim().minLength(8).regex(new RegExp('^(?=.*[A-Z])(?=.*\\d)(?=.*[\\W_]).{8,}$'))
   })
 )
