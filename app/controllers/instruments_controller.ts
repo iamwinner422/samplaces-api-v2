b67e566906a76be6eae8b14927ead7a89482f601
iamwinner422 <iamwinner422@gmail.com>
2024-03-30 10:57:51 +0000

    finition des routes

--- app/controllers/instruments_controller.ts
+++ app/controllers/instruments_controller.ts
@@ -3,7 +3,7 @@ import {AddValidator} from "#validators/instrument";
 import Instrument from "#models/instrument";
 
 export default class InstrumentsController {
-	async all({request, response}: HttpContext){
+	async all({response}: HttpContext){
 		const instruments: Instrument[] = await Instrument.query().orderBy('name', 'asc')
 		return response.status(200).send({success: true, data: instruments})
 	}
