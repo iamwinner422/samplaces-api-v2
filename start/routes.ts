db727978401a242152066e15a220f35a0be12ad2
iamwinner422 <iamwinner422@gmail.com>
2024-03-30 22:49:59 +0000

    Ajout des routes

--- start/routes.ts
+++ start/routes.ts
@@ -10,7 +10,7 @@ import router from '@adonisjs/core/services/router'
 import { middleware } from "#start/kernel";
 const AuthController = () => import('#controllers/auth_controller')
 const InstrumentsController = () => import('#controllers/instruments_controller')
-
+const SongsController = ()=> import('#controllers/songs_controller')
 
 router.get('/', ({response}) => {
 	response.redirect().toPath('/api')
@@ -30,9 +30,17 @@ router.group(() => {
 			router.put('/:id', [InstrumentsController, 'update'])
 			router.delete('/:id', [InstrumentsController, 'destroy'])
 		}).where('id', {match: /^[0-9]+$/})
-
-
 	}).prefix('instruments').use(middleware.auth())
 
+	router.group(() => {
+		router.get('/', [SongsController, 'index'])
+		router.post('/', [SongsController, 'store'])
+		router.group(() => {
+			router.get('/:id', [SongsController, 'show'])
+			router.put('/:id', [SongsController, 'update'])
+			router.put('/:id/:action', [SongsController, 'trashRestore'])
+			router.delete('/:id', [SongsController, 'destroy'])
+		}).where('id', {match: /^[0-9]+$/})
+	}).prefix('songs').use(middleware.auth())
 
 }).prefix('api')
