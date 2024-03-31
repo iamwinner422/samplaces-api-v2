7a9a0fca2b2de2777dce8a0860c09b06d8a6842d
iamwinner422 <iamwinner422@gmail.com>
2024-03-31 13:43:34 +0000

    Correction de la requete de selection

--- app/controllers/songs_controller.ts
+++ app/controllers/songs_controller.ts
@@ -8,8 +8,8 @@ export default class SongsController {
 	async index({response, auth}: HttpContext){
 		const user = await auth.authenticate()
 		const songs: Song[] = await Song.query().with('tracks', (query) => {
-			query.from('tracks').select('*')
-		}).select('*').from('tracks').where('user_id', user.id).orderBy('name', 'asc')
+			query.from('tracks').select('*').orderBy('name', 'asc')
+		}).where('user_id', user.id).orderBy('title', 'asc')
 		return response.status(200).send({success: true, data: songs})
 	}
 
