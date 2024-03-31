e9285bf4bf5561673063970b6e58f0d19f1d148c
iamwinner422 <iamwinner422@gmail.com>
2024-03-31 15:06:37 +0000

    ajout de la validation de track

--- app/validators/track.ts
+++ app/validators/track.ts
@@ -1,1 +1,10 @@
 import vine from '@vinejs/vine'
+
+export const AddValidator = vine.compile(
+	vine.object({
+		name: vine.string().trim().minLength(2).maxLength(254),
+		sampleOrPreset: vine.string().trim().minLength(2),
+		instrument_id: vine.number(),
+		song_id: vine.number(),
+	})
+)
