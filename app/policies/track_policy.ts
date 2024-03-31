a0cf5866694d100805527e26070f2bbe4e5464b7
iamwinner422 <iamwinner422@gmail.com>
2024-03-31 14:40:26 +0000

    finition de la policy de track

--- app/policies/track_policy.ts
+++ app/policies/track_policy.ts
@@ -1,8 +1,32 @@
 import User from '#models/user'
 import Track from '#models/track'
+import Song from '#models/song'
 import { BasePolicy } from '@adonisjs/bouncer'
 import { AuthorizerResponse } from '@adonisjs/bouncer/types'
 
 export default class TrackPolicy extends BasePolicy {
-  
+	/**
+	 * Every logged-in user can create
+	 */
+	create(user: User): AuthorizerResponse {
+		return true
+	}
+
+	readOne(user: User, track: Track, song: Song): AuthorizerResponse {
+		return user.id === song.userId && song.id === track.songId
+	}
+
+	/**
+	 * Only the post creator can edit
+	 */
+	edit(user: User, track: Track, song: Song): AuthorizerResponse {
+		return user.id === song.userId && song.id === track.songId
+	}
+
+	/**
+	 * Only the track creator can delete
+	 */
+	delete(user: User, track: Track, song: Song): AuthorizerResponse {
+		return user.id === song.userId && song.id === track.songId
+	}
 }
