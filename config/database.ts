5871499905c4b0e8a676baab6bd8e8f476f59a2d
iamwinner422 <iamwinner422@gmail.com>
2024-04-16 16:50:50 +0000

    Ajout de nouvel url

--- config/database.ts
+++ config/database.ts
@@ -7,6 +7,7 @@ const dbConfig = defineConfig({
     postgres: {
       client: 'pg',
       connection: {
+        connectionString: env.get('DB_CONNECTION_URL'),
         host: env.get('DB_HOST'),
         port: env.get('DB_PORT'),
         user: env.get('DB_USER'),
